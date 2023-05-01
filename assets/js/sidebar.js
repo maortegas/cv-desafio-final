5cccec253fdd7b2cdab5fe0c15f7d18314922f36
Felipe Saavedra <fisaavedrae@gmail.com>
2023-05-01 02:05:27 -0400

    version con fondo paisaje

---
+++ assets/js/sidebar.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict'
+    var tooltipTriggerList = Array.prototype.slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) { new bootstrap.Tooltip(tooltipTriggerEl) })
+})()
